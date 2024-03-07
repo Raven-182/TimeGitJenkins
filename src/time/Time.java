ad3c865caac0f96505bb7c47a31c4b2f8bfcecb3
raven <raven@localhost>
2024-03-07 12:30:46 -0500

    "Added github"

--- src/time/Time.java
+++ src/time/Time.java
@@ -25,7 +25,7 @@ public class Time
 	{
 		
 		int totalSeconds = getTotalSeconds("10:10:10");
-		System.out.println("Total Seconds ="+totalSeconds);
+		System.out.println("**GH Total Seconds ="+totalSeconds);
 //		try
 //		{
 //			
